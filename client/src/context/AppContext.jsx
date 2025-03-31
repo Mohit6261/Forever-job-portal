34496acba882eaba3274518a84cdd86ebafbab96
Mohit6261 <105044988+Mohit6261@users.noreply.github.com>
2025-03-31 22:28:39 +0530

    Update AppContext.jsx
    ok

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import axios from 'axios'
+import axios from 'axios';
 import { useAuth, useUser } from "@clerk/clerk-react";
 
 export const AppContext=createContext();
